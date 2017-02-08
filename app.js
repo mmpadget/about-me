0ece264fc298f5bcb820806b5abe4700365a10ad
Michael Padget <mpadget@comcast.net>
2017-02-08 15:35:39 -0800

    Q and A added to Console

--- app.js
+++ app.js
@@ -29,43 +29,49 @@ Answers
 5. Narp, Michael doesn't drive to class; he rides the bus.
 */
 
+// Question 1
 var quizResponseOne = prompt('Please Answer with Y or N. Does Michael really like movies?');
 
-//With prompt, will store info as a string.
 if(quizResponseOne.toUpperCase() === 'Y'){
-  console.log('Awesome! You are correct!');
+  console.log('Yes, Michael really likes movies.');
 } else {
-  console.log('Too Bad, you were wrong. Adam has a dog');
+  console.log('No, Michael really likes movies.');
 }
 
+// Question 2
 var quizResponseTwo = prompt('Please Answer with Y or N. Does Michael enjoy playing video games?');
 
 if(quizResponseTwo.toUpperCase() === 'Y'){
-  console.log('Awesome! You are correct!');
+  console.log('Yes, Michael enjoys playing video games.');
 } else {
-  console.log('Too Bad, you were wrong. Adam has a dog');
+  console.log('Surprise, you\'re wrong! Michael enjoys playing video games.');
 }
 
+// Question 3
 var quizResponseThree = prompt('Please Answer with Y or N. Does Michael own dogs?');
 
 if(quizResponseThree.toUpperCase() === 'Y'){
-  console.log('Awesome! You are correct!');
+  console.log('Sorry, that\'s not right. Michael, doesn\'t own dogs.');
 } else {
-  console.log('Too Bad, you were wrong. Adam has a dog');
+  console.log('Correct. Michael doesn\'t own dogs. Bonus: he does own two cats.');
 }
 
+// Question 4
 var quizResponseFour = prompt('Please Answer with Y or N. Does Michael love music?');
 
 if(quizResponseFour.toUpperCase() === 'Y'){
-  console.log('Awesome! You are correct!');
+  console.log('Yes, Michael loves music.');
 } else {
-  console.log('Too Bad, you were wrong. Adam has a dog');
+  console.log('Oops, that\'s incorrect. Who doesn\'t love music?');
 }
 
+// Question 5
 var quizResponseFive = prompt('Please Answer with Y or N. Does Michael drive to class?');
 
 if(quizResponseFive.toUpperCase() === 'Y'){
-  console.log('Awesome! You are correct!');
+  console.log('Narp, Michael doesn\'t drive to class; he rides the bus.');
 } else {
-  console.log('Too Bad, you were wrong. Adam has a dog');
+  console.log('That\'s the right answer. Michael braves the bus commute every day.');
 }
+
+// The End.
