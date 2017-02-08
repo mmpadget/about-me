3eb6973389dba2b719d019e151efbabc0a51b365
Michael Padget <mpadget@comcast.net>
2017-02-08 15:53:45 -0800

    Tested Q's, Updated comments

--- app.js
+++ app.js
@@ -2,32 +2,14 @@
 
 //Prompt the user 5 different times about you.
 
+// Play a game message
 var confirmQuiz = confirm('Hello, would you like to take a quiz?');
 
-//Handed boolean value, so no need to validate.
 if(confirmQuiz){
   console.log('Awesome! Lets get started!');
 } else {
   console.log('Too Bad, starting anyway.');
 }
-
-/*
-Questions
-1. Does Michael really like movies?
-2. Does Michael enjoy playing video games?
-3. Does Michael own dogs?
-4. Does Michael love music?
-5. Does Michael drive to class?
-*/
-
-/*
-Answers
-1. Yes, Michael really likes movies.
-2. Yes, Michael enjoys playing video games.
-3. Heck no! Michael doesn't own dogs, he has two cats.
-4. Yes, Michael loves music.
-5. Narp, Michael doesn't drive to class; he rides the bus.
-*/
 
 // Question 1
 var quizResponseOne = prompt('Please Answer with Y or N. Does Michael really like movies?');
