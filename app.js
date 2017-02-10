66db47757622b6090dc46644a21bfa53ab5c5b75
Michael Padget <mpadget@comcast.net>
2017-02-09 17:41:16 -0800

    Working numeric input

--- app.js
+++ app.js
@@ -1,4 +1,70 @@
-/*'use strict';
+'use strict';
+var firstName; // Prompt, string to boolean Y/N.
+var takeQuiz; // Boolean Y/N, if no too bad.
+
+// Questions 1-5.
+var q1 = 'Does Michael really like movies?'; // Prompt, string to boolean Y/N.
+var q2 = 'Does Michael enjoy playing video games?';
+var q3 = 'Does Michael own dogs?';
+var q4 = 'Does Michael love music?';
+var q5 = 'Does Michael drive to class?';
+
+// Answer correct reponse 1-5. Yes, true.
+var a1 = 'Correct, Michael really likes movies.'; // Prompt, string to boolean Y/N.
+var a2 = 'Correct, Michael enjoys playing video games.';
+var a3 = 'Correct, Michael doesn\'t own dogs.';
+var a4 = 'Correct, Michael loves music';
+var a5 = 'Correct, Michael doesn\'t drive to class.';
+
+// Wrong answer response 1-5. No, false.
+var w1 = 'Incorrect, Michael really likes movies.'; // Prompt, string to boolean Y/N.
+var w2 = 'Incorrect, Michael does enjoy playing video games.';
+var w3 = 'Incorrect, Michael doesn\'t own dogs.';
+var w4 = 'Incorrect, Michael does love music.';
+var w5 = 'Incorrect, Michael doesn\'t drive to class';
+
+// Guess the number of years I have worked in Retail Technology.
+var yearsGuess; // Prompt, string to integer.
+var yearsAnswer = '10';
+var answerGuesses = 4;
+var wrongGuesses; // Varialbe to hold how many wrong answers.
+
+// Test condition. Indicates to the user whether the guess is 'too high' or 'too low.
+for (var i = 0; i < 4; i++) {
+  var yearsGuess = prompt('How many years I\'ve worked in retail technology?');
+  if (yearsGuess === yearsAnswer) {
+    console.log('Correct');
+    console.log(alert('You got it right.'));
+    break;
+  }
+  else {
+    if (yearsGuess < 10) {
+      console.log(alert('Guess is too low.'));
+    } else {
+      console.log(alert('Guess is too high.'));
+    }
+    console.log(alert('Incorrect. You have ' + (answerGuesses -= 1) + ' guesses left.'));
+    // If incorrect, or false, run this code. Keep asking again until out of questions.
+  }
+}
+
+//var companiesQuestion;
+// var companiesWorked = [Apple, Google, Microsoft, Amazon, LinkedIn, Facebook, Twitter, Hecker];
+// var companiesCorrect;
+//
+// var totalQuestions; // Integer. Tally of total number of all questions.
+// var totalCorrectAnswers; // Integer. Total number of correct answers given thus far.
+// var totalWrongAnswers; // Integer. Tracks how many wrong answers. Wrong guesses less than 6.
+//
+// //var allQuestions = [1, 2, 3, 4, 5, 6, 7];
+// var answersYes = [];
+// var answersNo = [];
+
+// var companiesQuestion = prompt('Guess which companies I have worked for. You have six guesses.');
+
+// Upon using up those tries OR getting a correct answer, displays a message to the user indicating all of the possible correct answers.
+
+/*
 //Prompt the user 5 different times about you.
 var userName = prompt('Welcome. What is your first name?');
 
